560b3171f37de2868c9018db81e3aadc5007e89c
Dekan <dekanbrown@odyssy.io>
2024-02-26 15:51:08 -0700

    start on signal sesh display

--- src/pages/SigSesh.tsx
+++ src/pages/SigSesh.tsx
@@ -1,17 +1,66 @@
+import { BsPlusLg } from "react-icons/bs";
 
-import React from "react";
-import { useCurrentDao, useDaoData } from "@daohaus/moloch-v3-hooks";
-import { ParLg, ParSm, SingleColumnLayout } from "@daohaus/ui";
+import { ProposalList } from "@daohaus/moloch-v3-macro-ui";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  H2,
+  Link,
+  ParMd,
+  SingleColumnLayout,
+} from "@daohaus/ui";
 
+import { useParams } from "react-router-dom";
+import { styled } from "styled-components";
+import { Link as RouterLink } from "react-router-dom";
 
+import { EthAddress } from "@daohaus/utils";
+import { ValidNetwork } from "@daohaus/keychain-utils";
+
+import { useDaoData } from "@daohaus/moloch-v3-hooks";
+import { ListTcr, useTcrList } from "../hooks/useTcrList";
+import { SignalItem } from "../components/SignalItem";
+
+const LinkBox = styled.div`
+  display: flex;
+  gap: 2rem;
+  margin: 8rem 0;
+`;
+
+const SlimParMd = styled(ParMd)`
+  width: 70%;
+`;
+
+const StyledRouterLink = styled(RouterLink)`
+  text-decoration: none;
+`;
+
+// href={"https://signal.daohaus.fun/#/0xa/0xf5d6b637a9185707f52d40d452956ca49018247a"}
 export const SigSesh = () => {
-  const { daoChain } = useCurrentDao();
-  const { dao } = useDaoData();
+  const { daoChain, daoId } = useParams();
+  const { dao } = useDaoData({
+    daoId: daoId as EthAddress,
+    daoChain: daoChain as ValidNetwork,
+  });
+  const { tcrList } = useTcrList({
+    daoId: daoId as EthAddress,
+  });
 
   return (
-    <SingleColumnLayout title="Community Polls">
-      <ParLg>Collector Signal Sessions</ParLg>
-      <ParSm>Community initiated Polls to help prioritize and guide the currators.</ParSm>
+    <SingleColumnLayout>
+      <H2>{dao?.name} Signals</H2>
+
+      {tcrList &&
+        tcrList.map((tcr: ListTcr, i: number) => {
+          return <SignalItem tcr={tcr} key={i} />;
+        })}
+      <LinkBox>
+        <Link href={`https://signal.daohaus.fun/#/${daoChain}/${daoId}/create`}>
+          Create Signal
+        </Link>
+      </LinkBox>
     </SingleColumnLayout>
   );
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
+import { Keychain } from "@daohaus/keychain-utils";
 
 export const NFT_DAO_REFERRER = "DHNFTCuratorShamanSummonerV0.2"; 
 // NFT shaman name NFTCuratorShamanV0.2
@@ -5,3 +6,14 @@ export const NFT_DAO_REFERRER = "DHNFTCuratorShamanSummonerV0.2";
 export const DEFAULT_NETWORK_ID = "0xaa36a7";
 
 export const SUMMONER_URL = "https://dinhaus.github.io/din-summoner/";
+
+export const TCR_GRAPH_URL: Keychain = {
+    "0x1": "https://api.thegraph.com/subgraphs/name/hausdao/dh-signal-tcr",
+    "0x5": "https://api.thegraph.com/subgraphs/name/hausdao/dh-signal-tcr-goerli",
+    "0xa": "https://api.thegraph.com/subgraphs/name/hausdao/dh-signal-tcr-optimism",
+    "0x64":
+      "https://api.thegraph.com/subgraphs/name/hausdao/dh-signal-tcr-gnosis",
+    "0xaa36a7": "https://api.thegraph.com/subgraphs/name/hausdao/dh-signal-tcr-sepolia",
+  };
+
+export const DEFAULT_GRAPH_URL = "https://api.thegraph.com/subgraphs/name/daohaus/sepolia";
