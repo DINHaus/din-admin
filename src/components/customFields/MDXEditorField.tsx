3cf2f759a4295cbf94c004e0b9fb72af3ae98d9e
growindiedev <rabhishekranjan@gmail.com>
2024-04-03 22:20:37 +0530

    fix focus on container and overflow problem, add link plugin

--- src/components/customFields/MDXEditorField.tsx
+++ src/components/customFields/MDXEditorField.tsx
@@ -1,61 +1,81 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
+import { Buildable, Field } from "@daohaus/ui";
+import { useFormContext } from "react-hook-form";
+
 import {
-    Buildable,
-
-    Field,
-
-} from '@daohaus/ui';
-import { useFormContext } from 'react-hook-form';
-
-import { MDXEditor, headingsPlugin, UndoRedo, BoldItalicUnderlineToggles, 
-    toolbarPlugin, BlockTypeSelect, quotePlugin, listsPlugin, ListsToggle, MDXEditorMethods } from '@mdxeditor/editor'
-import '@mdxeditor/editor/style.css'
-import styled from 'styled-components';
+	MDXEditor,
+	headingsPlugin,
+	UndoRedo,
+	BoldItalicUnderlineToggles,
+	toolbarPlugin,
+	BlockTypeSelect,
+	quotePlugin,
+	listsPlugin,
+	ListsToggle,
+	MDXEditorMethods,
+	linkPlugin,
+	linkDialogPlugin,
+	CreateLink,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+import styled from "styled-components";
 
 const MarkDownContainer = styled.div`
-  padding: 10px;
-  margin-bottom: 5rem;
-  border-radius: 5px;
-  background-color: hsl(228, 43.3%, 17.5%); // TODO: use theme
-  font-family: inherit;
-  height: 50vh;
+	padding: 10px;
+	margin-bottom: 5rem;
+	border-radius: 5px;
+	background-color: hsl(228, 43.3%, 17.5%); // TODO: use theme
+	font-family: inherit;
+	min-height: 10vh;
+	max-height: 50vh;
+	overflow: auto;
 `;
 
 export const MDXEditorField = (props: Buildable<Field>) => {
-    const { setValue, watch } = useFormContext();
-    const [content, createdAt] = watch([props.id, "createdAt"]);
-    const ref = useRef<MDXEditorMethods>(null)
+	const { setValue, watch } = useFormContext();
+	const [content, createdAt] = watch([props.id, "createdAt"]);
 
-    const handleOnChange = (value: string) => {
-        setValue(props.id, value);
-    }
+	const ref = useRef<MDXEditorMethods>(null);
 
-    useEffect(() => {
-        const drafts = localStorage.getItem("drafts") || "{}" as string;
-        const parsedDrafts = JSON.parse(drafts);
+	const handleOnChange = (value: string) => {
+		setValue(props.id, value);
+	};
 
-        if (parsedDrafts[createdAt]) {
-            ref.current?.setMarkdown(parsedDrafts[createdAt]?.content || "")
-            setValue(props.id, parsedDrafts[createdAt]?.content);
+	useEffect(() => {
+		const drafts = localStorage.getItem("drafts") || ("{}" as string);
+		const parsedDrafts = JSON.parse(drafts);
 
-        }
+		if (parsedDrafts[createdAt]) {
+			ref.current?.setMarkdown(parsedDrafts[createdAt]?.content || "");
+			setValue(props.id, parsedDrafts[createdAt]?.content);
+		}
+	}, [createdAt, ref]);
 
-      }, [createdAt, ref]);
-
-    return (
-        <MarkDownContainer>
-            <MDXEditor ref={ref} className="dark-theme dark-editor" markdown={""} plugins={[listsPlugin(), quotePlugin(), headingsPlugin(), toolbarPlugin({
-                toolbarContents: () => (
-                    <>
-                        {' '}
-                        <UndoRedo />
-                        <BoldItalicUnderlineToggles />
-                        <ListsToggle />
-                        {' '}
-                        <BlockTypeSelect />
-                    </>
-                )
-            })]} onChange={handleOnChange} />
-        </MarkDownContainer>
-    );
+	return (
+		<MarkDownContainer>
+			<MDXEditor
+				ref={ref}
+				className="dark-theme dark-editor"
+				markdown={""}
+				plugins={[
+					listsPlugin(),
+					quotePlugin(),
+					headingsPlugin(),
+					linkPlugin(),
+					linkDialogPlugin(),
+					toolbarPlugin({
+						toolbarContents: () => (
+							<>
+								<UndoRedo />
+								<BoldItalicUnderlineToggles />
+								<CreateLink />
+								<ListsToggle /> <BlockTypeSelect />
+							</>
+						),
+					}),
+				]}
+				onChange={handleOnChange}
+			/>
+		</MarkDownContainer>
+	);
 };
